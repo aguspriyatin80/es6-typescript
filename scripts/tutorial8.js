86b504b9643ba3b58d4efef34a53b394b0a34cd2
Agus Priyatin <aguspriyatin80>
2021-08-18 03:39:36 +0700

    part08 - const keyword

---
+++ scripts/tutorial8.js
@@ -0,0 +1,12 @@
+"use strict";
+var num1;
+var num2 = 10;
+var obj1 = {
+    name: "Agus"
+};
+// obj1 = {
+//     name: "Eka"
+// }
+console.log(obj1.name);
+obj1.name = "Eka";
+console.log(obj1.name);
---
+++ src/tutorial8.ts
@@ -0,0 +1,12 @@
+let num1;
+const num2 = 10;
+
+const obj1 = {
+    name:"Agus"
+}
+// obj1 = {
+//     name: "Eka"
+// }
+console.log(obj1.name);
+obj1.name = "Eka";
+console.log(obj1.name);
