9e6bf1e2873511357a924e864e7938f51d45117a
Agus Priyatin <aguspriyatin80>
2021-08-20 02:31:52 +0700

    part16 - object literal 2

---
+++ scripts/tutorial16.js
@@ -0,0 +1,9 @@
+"use strict";
+var _a;
+var ln = "last name";
+var person = (_a = {
+        "first name": "Agus"
+    },
+    _a[ln] = "Priyatin",
+    _a);
+console.log(person);
---
+++ src/tutorial16.ts
@@ -0,0 +1,7 @@
+let ln = "last name";
+let person = {
+    "first name": "Agus",
+    [ln]: "Priyatin"
+}
+
+console.log(person);
