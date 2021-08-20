6815e7333fab965af925abcff3d32a1359a625e8
Agus Priyatin <aguspriyatin80>
2021-08-20 14:06:04 +0700

    part21 - class

---
+++ scripts/tutorial21.js
@@ -0,0 +1,13 @@
+"use strict";
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function () { };
+    return Person;
+}());
+var p = new Person();
+// console.log(p);
+console.log(p.greet === Person.prototype.greet);
+// employee(); // hoisting
+// function employee() { }
+// console.log(typeof employee);
---
+++ src/tutorial21.ts
@@ -0,0 +1,11 @@
+class Person {
+    greet() { }
+}
+
+const p = new Person();
+// console.log(p);
+console.log(p.greet === Person.prototype.greet);
+
+// employee(); // hoisting
+// function employee() { }
+// console.log(typeof employee);
