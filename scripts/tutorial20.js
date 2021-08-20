873bade2b0b6bae1eeac28ccf4142e5919ccbc31
Agus Priyatin <aguspriyatin80>
2021-08-20 13:48:57 +0700

    part20 - for of loops

---
+++ scripts/tutorial20.js
@@ -0,0 +1,16 @@
+"use strict";
+var colors = ["red", "green", "yellow"];
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+// for (let i in colors) {
+//     console.log(colors[i]);
+// }
+// for (let i of colors) {
+//     console.log(i);
+// }
+var str = "123";
+for (var _i = 0, str_1 = str; _i < str_1.length; _i++) {
+    var s = str_1[_i];
+    console.log(s);
+}
---
+++ src/tutorial20.ts
@@ -0,0 +1,17 @@
+let colors = ["red", "green", "yellow"];
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+// for (let i in colors) {
+//     console.log(colors[i]);
+// }
+
+// for (let i of colors) {
+//     console.log(i);
+// }
+let str = "123";
+
+for (let s of str) {
+    console.log(s);
+}
