13390f7e327aae86118b481f976ceed4019d91e0
Agus Priyatin <aguspriyatin80>
2021-08-21 14:05:57 +0700

    fixed

--- src/moduleA.js
+++ src/moduleA.js
@@ -3,4 +3,4 @@ import {fname, lname, obj} from './moduleB.js'  // log Module B laoded
 
 obj.name = "Ross";
 console.log(obj.name);
-console.log(` ${fname} ${lname}`);
+console.log(`${fname} ${lname}`);
